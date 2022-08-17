8b9f92fe8e0c9c089ecb0641d6c689f773d41f24
chimmyJay <chimmyJay@outlook.com>
2022-08-18 00:14:12 +0800

    prevent preflight by nginx

---
+++ prevent_preflight_request_by_nginx/site.js
@@ -0,0 +1,7 @@
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open("put", "http://api.testWeb.com/api/test"); 
+xmlHttp.send(); 
+
+var xmlHttp2 = new XMLHttpRequest();
+xmlHttp2.open("put", "http://testWeb.com/api/test"); 
+xmlHttp2.send(); 
